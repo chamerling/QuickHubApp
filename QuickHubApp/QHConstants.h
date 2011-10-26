a06e2b1b34d5ccaa2593f40422fbe8af52599007
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-10-26 23:27:30 +0200

    introducing constants

---
+++ QuickHubApp/QHConstants.h
@@ -0,0 +1,21 @@
+//
+//  QHConstants.h
+//  QuickHubApp
+//
+//  Created by Christophe Hamerling on 26/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QHConstants : NSObject
+
+#ifndef QH_Constants_h
+#define QH_Constants_h
+
+#define appsite     @"http://quickhubapp.com"
+#define eventDatePattern  @"yyyy-MM-dd"
+
+#endif
+
+@end

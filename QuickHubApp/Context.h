9279e0627452a22d31db9b761921f61940c9c1ea
Christophe Hamerling <christophe.hamerling@gmail.com>
2011-11-25 23:56:26 +0100

    Adding remaining API calls to MAS preferences

---
+++ QuickHubApp/Context.h
@@ -0,0 +1,13 @@
+//
+//  Context.h
+//  QuickHub
+//
+//  Created by Christophe Hamerling on 25/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Context : NSObject
+
+@end
---
+++ QuickHubApp/Context.m
@@ -0,0 +1,23 @@
+//
+//  Context.m
+//  QuickHub
+//
+//  Created by Christophe Hamerling on 25/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Context.h"
+
+@implementation Context
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end

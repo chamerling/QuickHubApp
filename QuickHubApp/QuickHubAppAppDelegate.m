bd5eeff26767cb07c3ff29f95c349d89e89136bc
Christophe Hamerling <christophe.hamerling@gmail.com>
2013-11-14 20:53:00 +0100

    LICENSE

--- QuickHubApp/QuickHubAppAppDelegate.m
+++ QuickHubApp/QuickHubAppAppDelegate.m
@@ -1,16 +1,25 @@
+// (The MIT License)
 //
-//  QuickHubAppAppDelegate.m
-//  QuickHubApp
+// Copyright (c) 2013 Christophe Hamerling <christophe.hamerling@gmail.com>
 //
-//  Created by Christophe Hamerling on 10/10/11.
-//  Copyright 2011 chamerling.org. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// 'Software'), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
 //
-//  Permission is given to use this source code file, free of charge, in any
-//  project, commercial or otherwise, entirely at your risk, with the condition
-//  that any redistribution (in part or whole) of source code must retain
-//  this copyright and permission notice. Attribution in compiled projects is
-//  appreciated but not required.
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
 //
+// THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "QuickHubAppAppDelegate.h"
 #import "GistCreateWindowController.h"
